1e7cb3479f0ab3a56ca81a9a96ebc284ea0e9620
GGuenov <german.guenov@gmail.com>
2023-07-12 12:07:02 +0100

    Back listo

--- api/src/controllers/getTemperamentsData.js
+++ api/src/controllers/getTemperamentsData.js
@@ -4,28 +4,67 @@ const { Sequelize } = require("sequelize");
 
 const cargarTemperamentosDesdeAPI = async (req, res) => {
   try {
-    const response = await axios.get(`https://api.thedogapi.com/v1/breeds/`);
-    //console.log(response);
+    const API = await axios.get(`https://api.thedogapi.com/v1/breeds/`);
+    //console.log(API);
 
-    const data = response.data;
-    for (const object of data) {
-      const listaTemperamentos = object.temperament.split(", ");
-      console.log(listaTemperamentos);
-      // if (listaTemperamentos.leght === 0) {
-      //   throw new Error("can't get them");
-      // }
-
-      for (const temperamento of listaTemperamentos) {
-        console.log(temperamento);
-
-        await Temperament.create({ name: temperamento });
+    const allTemperaments = new Set();
+    API.data.forEach((dog) => {
+      if (dog.temperament) {
+        const temps = dog.temperament.split(", ");
+        temps.forEach((temp) => allTemperaments.add(temp));
       }
-    }
+    });
+    //console.log(allTemperaments);
+    const AllFields = Array.from(allTemperaments).map((tempName) => {
+      return {
+        name: tempName,
+      };
+    });
+    //console.log(AllFields);
 
-    res.status(200).json("temperaments cargados.");
+    for (const temper of AllFields) await Temperament.create(temper);
+    //console.log(registers);
+
+    console.log("cargados");
+    res.status(200).json("CARGADOS");
   } catch (error) {
-    console.error("Error al cargar los temperamentos:", error);
+    console.log("faltan o sobran?");
+    //res.status(500).json({ error: error.mesage });
   }
 };
 
 module.exports = cargarTemperamentosDesdeAPI;
+
+//   try {
+//     const response = await axios.get(`https://api.thedogapi.com/v1/breeds/`);
+//     //console.log(response);
+
+//     const data = response.data;
+
+//     const temperamentosUnicos = new Set();
+//     //console.log(temperamentosUnicos);
+//     for (const object of data) {
+//       if (object.hasOwnProperty("temperament")) {
+//         const listaTemperamentos = object.temperament.split(",");
+//         //console.log(listaTemperamentos);
+
+//         for (const temperamento of listaTemperamentos) {
+//           //     temperamentosUnicos.add(temperamento.trim());
+//           //     console.log(temperamentosUnicos);
+//           //   }
+//           //   //const temperamentArray = [...temperamentosUnicos];
+//           //   //console.log(temperamentArray);
+//           // }
+//           // for (const temperamento of temperamentosUnicos) {
+//           await Temperament.findAndCreate({
+//             where: { name: temperamento.trim() },
+//           });
+//         }
+//       }
+//     }
+
+//     res.status(200).json("temperaments cargados.");
+//   } catch (error) {
+//     console.error("Error al cargar los temperamentos");
+//   }
+// };
